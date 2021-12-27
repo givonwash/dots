7dd437b07decd9255eb77e1de041359ed8e3555b
givonwash <givonwash@gmail.com>
2021-12-27 12:31:44 -0800

    feat(plugin): add new telescope mappings and picker configs

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,65 +2,24 @@ return function()
     local nmap = require('keymaps').nmap
 
     nmap('<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>')
-    nmap('<leader>fc', '<cmd>lua require("telescope.builtin").commands()<cr>')
     nmap('<leader>fk', '<cmd>lua require("telescope.builtin").keymaps()<cr>')
     nmap('<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>')
     nmap('<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>')
     nmap('<leader>fm', '<cmd>lua require("telescope.builtin").man_pages()<cr>')
     nmap('<leader>fr', '<cmd>lua require("telescope.builtin").live_grep()<cr>')
-    nmap('<leader>fe', '<cmd>lua require("telescope.builtin").lsp_references()<cr>')
+    nmap(
+        '<leader>fw',
+        '<cmd>lua require("telescope.builtin").file_browser({ cwd = vim.fn.expand "%:p:h" })<cr>'
+    )
+    nmap('<leader>ft', '<cmd>lua require("telescope.builtin").builtin()<cr>')
 
     local actions = require 'telescope.actions'
 
     require('telescope').setup {
         defaults = {
-            vimgrep_arguments = {
-                'rg',
-                '--color=never',
-                '--no-heading',
-                '--with-filename',
-                '--line-number',
-                '--column',
-                '--smart-case',
-            },
             prompt_prefix = ':: 🐵 :: ',
             selection_caret = '→ ',
-            entry_prefix = '  ',
-            initial_mode = 'insert',
-            scroll_strategy = 'cycle',
-            selection_strategy = 'reset',
-            sorting_strategy = 'descending',
-            layout_strategy = 'horizontal',
-            layout_config = {
-                horizontal = { mirror = false },
-                vertical = { mirror = false },
-                width = 0.75,
-                prompt_position = 'bottom',
-                preview_cutoff = 120,
-            },
-            file_sorter = require('telescope.sorters').get_fzy_sorter,
-            file_ignore_patterns = {},
-            generic_sorter = require('telescope.sorters').get_fzy_sorter,
-            path_display = { 'absolute' },
-            winblend = 0,
-            border = {},
-            borderchars = {
-                '─',
-                '│',
-                '─',
-                '│',
-                '╭',
-                '╮',
-                '╯',
-                '╰',
-            },
-            color_devicons = true,
-            use_less = true,
-            set_env = nil,
-            file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-            grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-            qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-            buffer_previewer_maker = require('telescope.previewers').buffer_previewer_maker,
+            path_display = { 'smart' },
             mappings = {
                 i = {
                     ['<C-c>'] = false,
@@ -71,6 +30,18 @@ return function()
                     ['<C-j>'] = actions.move_selection_next,
                     ['<C-k>'] = actions.move_selection_previous,
                 },
+            },
+        },
+        pickers = {
+            find_files = {
+                hidden = true,
+            },
+            file_browser = {
+                hidden = true,
+                dir_icon = '🖿',
+            },
+            man_pages = {
+                sections = { 'ALL' },
             },
         },
         extensions = {
