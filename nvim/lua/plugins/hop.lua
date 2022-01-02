bd71691f7b324d01f93899b24986a683ad064914
givonwash <givonwash@gmail.com>
2022-01-02 13:38:08 -0600

    fix(plugin): remove inclusive_jump opts from hop-nvim mappings

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -3,22 +3,14 @@ return function()
     local nmap = keymap.nmap
     local omap = keymap.mapper('o', nil, false)
 
-    require('hop').setup {}
+    require('hop').setup()
 
     nmap(
         'f',
-        '<cmd>lua require("hop").hint_char1({ direction = require("hop.hint").HintDirection.AFTER_CURSOR, inclusive_jump = true })<cr>'
-    )
-    nmap(
-        'F',
-        '<cmd>lua require("hop").hint_char1({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR, inclusive_jump = true })<cr>'
-    )
-    nmap(
-        't',
         '<cmd>lua require("hop").hint_char1({ direction = require("hop.hint").HintDirection.AFTER_CURSOR })<cr>'
     )
     nmap(
-        'T',
+        'F',
         '<cmd>lua require("hop").hint_char1({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR })<cr>'
     )
     omap(
