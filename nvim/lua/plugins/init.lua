537a61dfbc471b28350090ac9d60a2f12ad05644
givonwash <givonwash@gmail.com>
2022-01-14 21:20:15 -0800

    feat(plugin): add lazy loading for nvim-tree.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -115,6 +115,7 @@ return require('packer').startup(function(use)
         'kyazdani42/nvim-tree.lua',
         requires = { 'kyazdani42/nvim-web-devicons' },
         config = require 'plugins.nvim-tree',
+        cmd = 'NvimTreeToggle',
     }
 
     --[[ efficiency plugins ==================================================]]
