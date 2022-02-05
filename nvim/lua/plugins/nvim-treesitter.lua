09b282a8fc5e1d4fdd7382de08e7428811efc6be
givonwash <givonwash@gmail.com>
2022-02-04 22:48:50 -0800

    chore(plugins): remove manual addition of norg ts parser

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,4 @@
 return function()
-    local parsers = require('nvim-treesitter.parsers').get_parser_configs()
-
-    parsers.norg = {
-        install_info = {
-            url = 'https://github.com/nvim-neorg/tree-sitter-norg',
-            files = { 'src/parser.c', 'src/scanner.cc' },
-            branch = 'main',
-        },
-    }
-
     require('nvim-treesitter.configs').setup {
         ensure_installed = 'maintained',
         autotag = {
